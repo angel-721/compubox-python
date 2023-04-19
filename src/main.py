e69d20d218fedd15f57a4abe58c4053c8ae42d32
angel-721 <angelvelasquez2002@gmail.com>
2023-04-19 09:25:49 -0600

    add add-fights

--- src/main.py
+++ src/main.py
@@ -6,8 +6,9 @@ import argparse
 def parseArgs():
     parser = argparse.ArgumentParser(prog="Compubox", description="A simple command line Compubox")
     parser.add_argument("--hint", "-hi", type=int, default=0)
-    parser.add_argument("--add-fighter", "-af", type=int, default=0)
+    parser.add_argument("--add-fighter", "-afr", type=int, default=0)
     parser.add_argument("--add-coach", "-ac", type=int, default=0)
+    parser.add_argument("--add-fight", "-af", type=int, default=0)
 
     # Test arguments here
     
@@ -21,16 +22,8 @@ def main(args):
     if(args.add_coach != 0):
         addCoach(args.hint)
 
-
-        
-
-
-
-
-
-
-
-
+    if(args.add_fight != 0):
+        addFight(args.hint)
 
 if __name__ == "__main__":
     main(parseArgs())
